88ba6d2636fcd44f1b21f3ae68fea1690149f3a5
KarlHarnois <karlrivestharnois@gmail.com>
2023-12-17 00:25:49 -0500

    Change route titles from Hydrogen to Antigone

--- app/routes/($locale).cart.tsx
+++ app/routes/($locale).cart.tsx
@@ -7,7 +7,7 @@ import {CartMain} from '~/components/Cart';
 import {useRootLoaderData} from '~/root';
 
 export const meta: MetaFunction = () => {
-  return [{title: `Hydrogen | Cart`}];
+  return [{title: `Antigone | Cart`}];
 };
 
 export async function action({request, context}: ActionFunctionArgs) {
